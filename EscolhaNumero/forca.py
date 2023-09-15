a3a10a8f12b7101bc20f6db94f1ec414883f6847
Eduardo Furlaneti <eduardohppa@gmail.com>
2023-09-15 14:52:26 -0300

    feat: Finalizando curso

--- EscolhaNumero/forca.py
+++ EscolhaNumero/forca.py
@@ -1,11 +1,17 @@
 # from faker import Faker
 import random
 
-def jogar():
-    print('****************************************************')
-    print('************ Bem vindo ao jogo de forca ************')
-    print('****************************************************')
+def imprime_mensagem(mensagem):
+    if(mensagem == 'abertura'):
+        print('****************************************************')
+        print('************ Bem vindo ao jogo de forca ************')
+        print('****************************************************')
+    if(mensagem == 'encerramento'):
+        print('****************************************************')
+        print('******************** GAME OVER *********************')
+        print('****************************************************')
 
+def seleciona_palavra_da_forca():
     # Código aqui
     # Aqui temos um gerador automático
     # fake = Faker('pt_BR')
@@ -20,54 +26,63 @@ def jogar():
     arquivo.close()
     
     numero_aleatorio = random.randrange(0, len(palavras_disponiveis))
-    word = palavras_disponiveis[numero_aleatorio].upper().strip()    
+    word = palavras_disponiveis[numero_aleatorio].upper().strip()  
+    return word  
 
-    # empty_spaces = []
+def resolve_espacos_vazios(palavra):
+    # espacos_vazios = []
     # for letra in word:
-    # empty_spaces.append('_' for letra in word) 
-    empty_spaces = ['_' for letra in word] #A lista é forma colocando um caracter para cada letra da palavra
+    # espacos_vazios.append('_' for letra in word) 
+    espacos_vazios = ['_' for letra in palavra] #A lista é forma colocando um caracter para cada letra da palavra
     #  Isso aqui é conhecido como List Comprehension
-    
+    return espacos_vazios
+
+def mensagem_final(encontrou_palavra, enforcado, palavra): 
+    if(encontrou_palavra):
+        print('PRABÉNS! Você venceu\nA palavra é {}'.format(palavra))
+
+    if(enforcado):
+        print('Você perdeu :(\nA palavra era {}'.format(palavra))
+
+def jogar():
+    imprime_mensagem('abertura')
+
+    word = seleciona_palavra_da_forca()
+    espacos_vazios = resolve_espacos_vazios(word)
     hang = False
-    win_word = False
+    acertou_palavra = False
     chances = 6
 
-    while(not hang and not win_word):
-        for caracter in empty_spaces:
+    while(not hang and not acertou_palavra):
+        for caracter in espacos_vazios:
             print(caracter, end = ' ')
 
-        play_char = input('\n\nChute uma letra: ')
-        play_char = play_char.strip().upper()
+        letra_chutada = input('\n\nChute uma letra: ')
+        letra_chutada = letra_chutada.strip().upper()
         index = 0
-        find = False
+        encontrou = False
         
         for letra in word: # Funciona quase como um Foreach do PHP
-            if(play_char == letra):
+            if(letra_chutada == letra):
                 print('Encontrei a letra {} na posicao {}'.format(letra, index))
-                empty_spaces[index] = letra
-                find = True
+                espacos_vazios[index] = letra
+                encontrou = True
             index += 1
 
-        if(not find):
+        if(not encontrou):
             chances -=1
             print("Você tem {} chances".format(chances))
         
-        if("_" not in empty_spaces):
-            win_word = True
+        if("_" not in espacos_vazios):
+            acertou_palavra = True
         
         if(chances == 0):
             hang = True
-    # Fim código
+    
+    mensagem_final(acertou_palavra, hang, word)
 
-    if(win_word):
-        print('PRABÉNS! Você venceu\nA palavra é {}'.format(word))
-
-    if(hang):
-        print('Você perdeu :(\nA palavra era {}'.format(word))
-
-    print('****************************************************')
-    print('******************** GAME OVER *********************')
-    print('****************************************************')
+    imprime_mensagem('encerramento')
+    
 
 if(__name__ == '__main__'):
     jogar()
